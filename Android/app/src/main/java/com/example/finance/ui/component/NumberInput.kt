0bd76c68fa49e990c1125487594398aa92e68200
Rohit Rai <raistudying@gmail.com>
2026-01-05 22:33:41 +0530

    Style text and number input field

--- Android/app/src/main/java/com/example/finance/ui/component/NumberInput.kt
+++ Android/app/src/main/java/com/example/finance/ui/component/NumberInput.kt
@@ -1,29 +1,43 @@
 package com.example.finance.ui.component
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import com.example.finance.R
+import com.example.finance.ui.theme.TextInputDarkColorScheme
 
 @Composable
-fun NumberInput(isError: MutableState<Boolean>, label: String, placeholder: String, state: TextFieldState) {
-    Column {
-        TextField(
+fun NumberInput(
+    isError: MutableState<Boolean>,
+    label: String,
+    placeholder: String,
+    state: TextFieldState
+) {
+    Column( modifier = Modifier.padding(16.dp) ) {
+        OutlinedTextField(
+            colors = TextInputDarkColorScheme,
             isError = isError.value,
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             label = { Text(if (isError.value) "$label*" else label) },
-            modifier = Modifier,
+            modifier = Modifier.fillMaxWidth(),
             placeholder = { Text(placeholder) },
+            shape = RoundedCornerShape(16.dp),
             state = state,
             supportingText = {
-                Text(if (isError.value) stringResource(R.string.add_error_message) else "")
+                Text(
+                    if (isError.value) stringResource(R.string.add_error_message) else ""
+                )
             },
         )
     }
--- Android/app/src/main/java/com/example/finance/ui/theme/Color.kt
+++ Android/app/src/main/java/com/example/finance/ui/theme/Color.kt
@@ -3,6 +3,11 @@ package com.example.finance.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Black = Color.Black
+val Gray = Color.Gray
+val LightGray = Color.LightGray
+val LighterRed = Color(0x22FF0000)
+val LightRed = Color(0x88FF0000)
+val LightWhite = Color(0x88FFFFFF)
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -10,4 +15,5 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val Red = Color.Red
 val White = Color.White
