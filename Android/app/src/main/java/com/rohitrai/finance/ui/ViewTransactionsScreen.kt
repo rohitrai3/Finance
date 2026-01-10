64a84b42f37f8aaf4f11d8c12c9750331adc698d
Rohit Rai <raistudying@gmail.com>
2026-01-10 10:03:18 +0530

    Keep new item at botton in view transactions

--- Android/app/src/main/java/com/rohitrai/finance/ui/ViewTransactionsScreen.kt
+++ Android/app/src/main/java/com/rohitrai/finance/ui/ViewTransactionsScreen.kt
@@ -1,5 +1,6 @@
 package com.rohitrai.finance.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -10,7 +11,10 @@ import java.util.Collections
 
 @Composable
 fun ViewTransactionsScreen(transactions: List<Transaction> = Collections.emptyList()) {
-    LazyColumn(state = rememberLazyListState(initialFirstVisibleItemIndex = Int.MAX_VALUE)) {
+    LazyColumn(
+        state = rememberLazyListState(initialFirstVisibleItemIndex = Int.MAX_VALUE),
+        verticalArrangement = Arrangement.Bottom
+    ) {
         itemsIndexed(transactions) { _, transaction ->
             ViewTransaction(transaction)
         }
