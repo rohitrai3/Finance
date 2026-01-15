dd225b35d25d0eabc5603c6a81e36549f8579fc2
Rohit Rai <raistudying@gmail.com>
2026-01-15 21:42:31 +0530

    Boundry around transaction view

--- Android/app/src/main/java/com/rohitrai/finance/ui/ViewTransactionsScreen.kt
+++ Android/app/src/main/java/com/rohitrai/finance/ui/ViewTransactionsScreen.kt
@@ -1,10 +1,13 @@
 package com.rohitrai.finance.ui
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.rohitrai.finance.data.Transaction
 import com.rohitrai.finance.ui.component.ViewTransaction
 import java.util.Collections
@@ -12,6 +15,7 @@ import java.util.Collections
 @Composable
 fun ViewTransactionsScreen(transactions: List<Transaction> = Collections.emptyList()) {
     LazyColumn(
+        modifier = Modifier.padding(0.dp, 4.dp),
         state = rememberLazyListState(initialFirstVisibleItemIndex = Int.MAX_VALUE),
         verticalArrangement = Arrangement.Bottom
     ) {
--- Android/app/src/main/java/com/rohitrai/finance/ui/component/ViewTransaction.kt
+++ Android/app/src/main/java/com/rohitrai/finance/ui/component/ViewTransaction.kt
@@ -1,10 +1,12 @@
 package com.rohitrai.finance.ui.component
 
 import android.icu.text.DateFormat
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,18 +17,22 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rohitrai.finance.R
 import com.rohitrai.finance.data.Transaction
+import com.rohitrai.finance.ui.theme.Gray
 import java.util.Date
 
 @Composable
 fun ViewTransaction(transaction: Transaction) {
     val dateFormat = DateFormat.getDateInstance()
     Column(
-    modifier = Modifier
-    .padding(2.dp)
-    .fillMaxWidth()
+        modifier = Modifier
+            .padding(4.dp, 4.dp)
+            .border(2.dp, Gray, RoundedCornerShape(16.dp))
+            .fillMaxWidth()
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp, 16.dp, 16.dp, 0.dp)
         ) {
             Text(
                 fontSize = 16.sp,
@@ -37,8 +43,8 @@ fun ViewTransaction(transaction: Transaction) {
                 text = transaction.amount.toString()
             )
             Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.End
+                horizontalAlignment = Alignment.End,
+                modifier = Modifier.fillMaxWidth()
             ) {
                 Text(
                     fontSize = 16.sp,
@@ -51,6 +57,9 @@ fun ViewTransaction(transaction: Transaction) {
                 )
             }
         }
-        Text(transaction.description)
+        Text(
+            modifier = Modifier.padding(16.dp, 0.dp, 16.dp, 16.dp),
+            text = transaction.description
+        )
     }
 }
