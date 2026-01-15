fb867a3bbc1102d4e8be2cc3b5725f5452203d6f
Rohit Rai <raistudying@gmail.com>
2026-01-15 20:26:11 +0530

    Move top bar content below status bar

--- Android/app/src/main/java/com/rohitrai/finance/ui/component/TopBar.kt
+++ Android/app/src/main/java/com/rohitrai/finance/ui/component/TopBar.kt
@@ -7,8 +7,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,7 +29,7 @@ fun TopBar(activity: Activity) {
         modifier = Modifier
             .background(DarkGray)
             .fillMaxWidth()
-            .height(48.dp),
+            .statusBarsPadding(),
         verticalAlignment = Alignment.Bottom
     ) {
         Text(
