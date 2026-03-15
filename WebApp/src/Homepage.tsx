5073f49057ec80c853561cb74f5aab69f10ed14e
Rohit Rai <rohitraijob@gmail.com>
2026-03-15 15:13:06 +0530

    refactor: Move add transaction form

--- WebApp/src/Homepage.tsx
+++ WebApp/src/Homepage.tsx
@@ -1,53 +1,8 @@
-import { Type, type AddTransactionInput } from "./types";
+import AddTransactionForm from "./components/AddTransactionForm";
 
 function Homepage() {
-  async function add(formData: FormData) {
-    const addTransactionInput: AddTransactionInput = {
-      amount: parseInt(formData.get("amount")?.toString()!),
-      type: formData.get("type")?.toString()!,
-      description: formData.get("description")?.toString()!,
-      tags: formData.get("tags")?.toString()!,
-      date: Date.parse(formData.get("date")?.toString()!)
-    };
-
-    await fetch("http://localhost:8080/transaction/add", {
-      body: JSON.stringify(addTransactionInput),
-      headers: {
-        "Content-Type": "application/json"
-      },
-      method: "POST"
-    })
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log("data: ", data);
-      })
-      .catch((err) => {
-        console.log("Error: ", err);
-      });
-  }
-
   return <div className="flex h-screen items-center justify-center w-screen">
-    <form action={add} className="flex flex-col gap-6 w-lg">
-      <input className="border border-white px-4 py-2 rounded-xl" name="amount" placeholder="Amount" type="text" />
-      <div className="flex">
-        <label className="flex flex-1 gap-2">
-          <input defaultChecked={true} name="type" type="radio" value={Type.DEBIT} />
-          Debit
-        </label>
-        <label className="flex flex-1 gap-2">
-          <input name="type" type="radio" value={Type.CREDIT} />
-          Credit
-        </label>
-      </div>
-      <input className="border border-white px-4 py-2 rounded-xl" name="description" placeholder="Description" type="text" />
-      <input className="border border-white px-4 py-2 rounded-xl" name="tags" placeholder="Tags" type="text" />
-      <input className="border border-white px-4 py-2 rounded-xl" name="date" type="date" />
-      <div className="flex justify-end">
-        <input className="bg-gray px-4 py-2 rounded-xl w-fit" type="submit" value="Add" />
-      </div>
-    </form>
+    <AddTransactionForm />
   </div>;
 }
 
