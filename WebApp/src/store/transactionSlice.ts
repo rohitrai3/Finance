5085ac94fefbc368a5637300af37a7ffde3aa0bc
Rohit Rai <rohitraijob@gmail.com>
2026-03-28 13:34:38 +0530

    feat: Get backend URL from env

--- WebApp/src/store/transactionSlice.ts
+++ WebApp/src/store/transactionSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { GetTransactionsResponse } from "../types";
 
 export const transactionApiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080/transaction/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_BACKEND_URL}transaction/` }),
   reducerPath: "transactionApi",
   tagTypes: ["Transaction"],
   endpoints: build => ({
